9f35f007d17fca9f3222b40a5c4add0f813aeca0
Junya <junya.ishibuchi@gmail.com>
2023-08-31 21:11:26 -0700

    fix: test codes actually didn't work

--- solutions/217_contains-duplicate.test.js
+++ solutions/217_contains-duplicate.test.js
@@ -3,10 +3,10 @@ import { containsDuplicate } from "./217_contains-duplicate";
 test.each([[[1, 2, 3, 1]], [[1, 1, 1, 3, 3, 4, 3, 2, 4, 2]]])(
   "true",
   (array) => {
-    expect(containsDuplicate(array)).toBeTruthy;
+    expect(containsDuplicate(array)).toBeTruthy();
   }
 );
 
 test.each([[[1, 2, 3]], [[1]], [[]]])("false", (array) => {
-  expect(containsDuplicate(array)).toBeTruthy;
+  expect(containsDuplicate(array)).toBeFalsy();
 });
