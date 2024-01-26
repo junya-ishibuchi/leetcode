e861fa3447a3d7b91c290e871a5f385a9b47c213
Junya <junya.ishibuchi@gmail.com>
2024-01-25 18:06:44 -0800

    refactor: easier to read

--- solutions/238_product-of-array-except-self.js
+++ solutions/238_product-of-array-except-self.js
@@ -12,31 +12,30 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  const left = generateLeftProduct(nums);
-  const right = generateRightProduct(nums);
+  const prefix = generatePrefixProduct(nums);
+  const postfix = generatePostfixProduct(nums);
 
   const results = [];
   for (let i = 0; i < nums.length; i++) {
-    results.push(left[i] * right[i]);
+    results[i] = (prefix[i - 1] ?? 1) * (postfix[i + 1] ?? 1);
   }
 
   return results;
 };
 
-function generateLeftProduct(nums) {
-  const products = [1];
-  for (let i = 1; i < nums.length; i++) {
-    products.push(products[i - 1] * nums[i - 1]);
+function generatePrefixProduct(nums) {
+  const products = [];
+  for (let i = 0; i < nums.length; i++) {
+    products[i] = (products[i - 1] ?? 1) * nums[i];
   }
 
   return products;
 }
 
-function generateRightProduct(nums) {
-  const products = new Array(nums.length);
-  products[nums.length - 1] = 1;
-  for (let i = nums.length - 1; i > 0; i--) {
-    products[i - 1] = products[i] * nums[i];
+function generatePostfixProduct(nums) {
+  const products = [];
+  for (let i = nums.length - 1; i >= 0; i--) {
+    products[i] = (products[i + 1] ?? 1) * nums[i];
   }
 
   return products;
